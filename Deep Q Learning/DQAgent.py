c8d508fdc0705201df7613c7c5d4056a0d4cc201
Blake Richey <blake.e.richey@gmail.com>
2019-07-10 15:24:10 -0500

    Added train, learn, and evaluate functions

--- Deep Q Learning/DQAgent.py
+++ Deep Q Learning/DQAgent.py
@@ -5,6 +5,8 @@ Created on Monday July 8, 2019
 @author: Blake Richey
 '''
 
+import cProfile, pstats, io
+
 import gym
 import os, datetime, random
 import numpy          as np
@@ -18,9 +20,9 @@ from   tensorflow.keras.layers     import Dense, Dropout, Conv2D, MaxPooling2D, 
     Activation, Flatten, BatchNormalization, LSTM
 
 # For more repetitive results
-random.seed(1)
-np.random.seed(1)
-tf.compat.v1.set_random_seed(1)
+# random.seed(1)
+# np.random.seed(1)
+# tf.compat.v1.set_random_seed(1)
 
                 
 class Utilities():
@@ -52,12 +54,41 @@ class Utilities():
         plt.plot(self.aggregate_episode_rewards['episode'], self.aggregate_episode_rewards['min'], label="min rewards")
         plt.legend(loc=4)
         plt.show()
+    
+    # This is a small utility for printing readable time strings:
+    def format_time(self, seconds):
+        if seconds < 400:
+            s = float(seconds)
+            return "%.1f seconds" % (s,)
+        elif seconds < 4000:
+            m = seconds / 60.0
+            return "%.2f minutes" % (m,)
+        else:
+            h = seconds / 3600.0
+            return "%.2f hours" % (h,)
 
 
 
 class DQAgent(Utilities):
 
     def __init__(self, env, model=None, **kwargs):
+        '''
+            Initialize agent hyperparameters
+
+            agent_opts = {
+                'BATCH_SIZE':              8,
+                'AGGREGATE_STATS_EVERY':   5,
+                'SHOW_EVERY':             10,
+                'EPSILON_START':         .98,
+                'EPSILON_DECAY':         .98,
+                'DISCOUNT':              .99,
+                'MAX_STEPS':             500,
+                'MIN_EPSILON' :          0.01,
+                'REPLAY_MEMORY_SIZE':    1000,
+                'LEARNING_RATE':         0.001,
+                'ACTION_POLICY':         'eg'
+            } 
+        '''
         assert isinstance(env,gym.wrappers.time_limit.TimeLimit),\
             "Environment should be a Gym environment."
 
@@ -67,19 +98,21 @@ class DQAgent(Utilities):
         # Hyperparameters
         self.batch_size    = kwargs.get('BATCH_SIZE',         8)     # How many steps (samples) to use for training
         self.max_steps     = kwargs.get('MAX_STEPS',          500)
-        self.epsilon_start = kwargs.get('EPSILON_START',      0.98)
-        self.discount      = kwargs.get('DISCOUNT',           0.99)
+        self.epsilon       = kwargs.get('EPSILON_START',      0.98)
+        self.epsilon_decay = kwargs.get('EPSILON_DECAY',      0.98)
+        self.discount      = kwargs.get('DISCOUNT',           0.99)  #HIGH VALUE = SHORT TERM MEMORY
         self.replay_size   = kwargs.get('REPLAY_MEMORY_SIZE', 1000)  #steps in memory
-        self.min_epsilon   = kwargs.get('MIN_EPSILON',        0.001)
+        self.min_epsilon   = kwargs.get('MIN_EPSILON',        0.01)
         self.learning_rate = kwargs.get('LEARNING_RATE',      0.001)
-        self.action_policy = kwargs.get('ACTION_POLICY',      'softmax')
+        self.action_policy = kwargs.get('ACTION_POLICY',      'eg')  #epsilon greedy
         
         # Data Recording Variables
         self.show_every            = kwargs.get('SHOW_EVERY',            10)
         self.aggregate_stats_every = kwargs.get('AGGREGATE_STATS_EVERY',  5)
 
         # Exploration settings    
-        self.explore_spec = {'EPSILON_DECAY': self.epsilon_start,
+
+        self.explore_spec = {'EPSILON_DECAY': self.epsilon_decay,
                              'MIN_EPSILON':   self.min_epsilon}
 
         # Memory
@@ -93,53 +126,81 @@ class DQAgent(Utilities):
             self.model = model.load_weights(self.weights_file)
 
     def build_model(self, model_type='dense', lr = 0.001, **kwargs):
-      if not hasattr(self, 'model'):
-        #define NN
-        self.num_outputs     = self.env.action_space.n 
-        self.num_layers      = kwargs.get('num_layers',      3)
-        self.default_nodes   = kwargs.get('default_nodes',   20)
-        self.nodes_per_layer = kwargs.get('nodes_per_layer', [])
-        self.dropout_rate    = kwargs.get('dropout_rate',    0.5)
-        self.add_dropout     = kwargs.get('add_dropout',     False)
-        self.activation      = kwargs.get('activation',      'linear')
-        self.num_features    = self.env.observation_space.shape[0]
+        '''
+            Builds model to be trained
 
-        #Create NN
-        if model_type == 'dense':
-          assert self.num_layers >=1, 'Number of layers should be greater than or equal to one!'
+            model_opts = {
+                'num_layers':      3,
+                'default_nodes':   20,
+                'dropout_rate':    0.5,
+                'add_dropout':     False,
+                'activation':      'linear',
+                'nodes_per_layer': [20,20,20],
+            }
+        '''
+        if not hasattr(self, 'model'):
+            #define NN
+            self.num_outputs     = self.env.action_space.n 
+            self.num_layers      = kwargs.get('num_layers',      3)
+            self.default_nodes   = kwargs.get('default_nodes',   20)
+            self.nodes_per_layer = kwargs.get('nodes_per_layer', [])
+            self.dropout_rate    = kwargs.get('dropout_rate',    0.5)
+            self.add_dropout     = kwargs.get('add_dropout',     False)
+            self.activation      = kwargs.get('activation',      'linear')
+            self.num_features    = self.env.observation_space.shape[0]
 
-          model = Sequential()
-          model.add(Dense(self.num_features, input_shape = (self.num_features,)))
-          
-          for layer in range(self.num_layers):
-  
-            try:
-              nodes=self.nodes_per_layer[layer]
-            except IndexError:
-              nodes = None
+            #Create NN
+            if model_type == 'dense':
+                assert self.num_layers >=1, 'Number of layers should be greater than or equal to one!'
 
-            if nodes is None:
-              nodes = self.default_nodes
+                model = Sequential()
+                model.add(Dense(self.num_features, input_shape = (self.num_features,)))
+                
+                for layer in range(self.num_layers):
+        
+                    try:
+                        nodes=self.nodes_per_layer[layer]
+                    except IndexError:
+                        nodes = None
 
-            print(layer)
-            model.add(Dense(units = nodes, activation = 'relu'))
-            print('Added Dense layer with ' + str(nodes) + ' nodes.')
-            if self.add_dropout:
-              model.add(Dropout(rate = self.dropout_rate, name='dropout_'+str(layer+1)))
-              print('Added Dropout to layer')
-          
-          #output layer
-          model.add(Dense(units = self.num_outputs, activation = self.activation, name='dense_output'))
-          model.compile(optimizer = Adam(lr=lr), loss = 'mse', metrics=['accuracy']) #Add loss for cross entropy?
-          model.summary()
+                    if nodes is None:
+                        nodes = self.default_nodes
+
+                    print(layer)
+                    model.add(Dense(units = nodes, activation = 'relu'))
+                    print('Added Dense layer with ' + str(nodes) + ' nodes.')
+                    if self.add_dropout:
+                        model.add(Dropout(rate = self.dropout_rate, name='dropout_'+str(layer+1)))
+                    print('Added Dropout to layer')
+                
+                #output layer
+                model.add(Dense(units = self.num_outputs, activation = self.activation, name='dense_output'))
+                model.compile(optimizer = Adam(lr=lr), loss = 'mse', metrics=['accuracy']) #Add loss for cross entropy?
+                model.summary() 
+
+        
+        self.model = model
+    
+    def evaluate(self, n_epochs=1):
+        print('Evaluating...')
+        for _ in range(n_epochs):
+            n_steps = 0
+            done = False
+            envstate = self.env.reset()
+            while (not done and n_steps < self.max_steps):
+                prev_envstate = envstate
+                q             = self.model.predict(prev_envstate.reshape(1, -1))
+                action        = np.argmax(q[0])
+                envstate, reward, done, info = self.env.step(action)
+                n_steps += 1
+                self.env.render()
 
       
-      self.model = model
-    
-    def evaluate(self):
-      pass
 
     def get_batch(self):
+        '''
+            Gets previous states to perform a batch fitting
+        '''
         mem_size   = len(self.memory)
         batch_size = min(mem_size, self.batch_size)
         env_size   = self.memory[0][0].reshape(1, -1).shape[1]
@@ -147,10 +208,11 @@ class DQAgent(Utilities):
         inputs = np.zeros((batch_size, env_size))
         targets = np.zeros((batch_size, self.num_outputs))
         for i, j in enumerate(np.random.choice(range(mem_size), batch_size, replace=False)):
-            envstate, action, reward, next_envstate, done = self.memory[j]
+            envstate, action, reward, next_envstate, done, target, Q_sa = self.memory[j]
             inputs[i] = envstate
-            targets[i] = self.predict(envstate)
-            Q_sa = np.max(self.predict(next_envstate))
+            # targets[i] = self.model.predict(envstate.reshape(1, -1))
+            targets[i] = target
+            # Q_sa = np.max(self.model.predict(next_envstate.reshape(1, -1)))
             if done:
                 targets[i, action] = reward
             else:
@@ -159,7 +221,16 @@ class DQAgent(Utilities):
         return inputs, targets
 
     def learn(self): #add callback options?
-      pass
+      inputs, targets = self.get_batch()
+      history = self.model.fit(
+          inputs,
+          targets,
+          batch_size = self.batch_size,
+          verbose=0,
+      )
+      loss = self.model.evaluate(inputs, targets, verbose=0)[0]
+
+      return loss
     
     def load_weights(self, filename):
       '''loads weights from a file'''
@@ -170,13 +241,17 @@ class DQAgent(Utilities):
             envstate: envstate to be evaluated
             returns:  given envstate, returns best action model believes to take
         '''
-        assert self.model, 'Model must be present to predict'
+        assert self.model, 'Model must be present to make prediction'
 
-        qvals = self.model.predict(envstate.reshape(1, -1))[0]
         if self.action_policy == 'softmax':
+            qvals = self.model.predict(envstate.reshape(1, -1))[0]
             action = np.argmax(np.random.multinomial(1, qvals))
-        else:
-            action = np.argmax(qvals)
+        elif self.action_policy == 'eg': #epsilon greedy
+            if np.random.rand() < self.epsilon:
+                action = random.choice(range(self.env.action_space.n))
+            else:
+                qvals = self.model.predict(envstate.reshape(1, -1))[0]
+                action = np.argmax(qvals)
 
         return action
 
@@ -185,10 +260,12 @@ class DQAgent(Utilities):
       'Add to replay buffer'
 
       envstate, action, reward, next_envstate, done = episode
+      target = self.model.predict(envstate.reshape(1, -1))
+      Q_sa   = np.max(self.model.predict(next_envstate.reshape(1, -1)))
       if reward > self.best_reward.get('Reward', 0):
         self.best_reward = {'Observation': next_envstate, 'Reward': reward}
       
-      self.memory.append(episode)
+      self.memory.append(episode + [target, Q_sa])
       if len(self.memory) > self.replay_size:
         del self.memory[0]
     
@@ -198,6 +275,49 @@ class DQAgent(Utilities):
       self.model.save_weights(h5file, overwrite=True)
       print('Weights saved to:', h5file)
     
-    def train(self, n_epochs=15000, max_steps=500):
-      self.start_time    = datetime.datetime.now()
-      pass
+    def train(self, n_epochs=15000, max_steps=0):
+        self.start_time    = datetime.datetime.now()
+        print(f'Starting training at {self.start_time}')
+        print(f'Action Decision Policy: {self.action_policy}')
+
+        max_steps = max_steps or self.max_steps
+        for epoch in range(n_epochs):
+            n_steps  = 0
+            done     = False
+            envstate = self.env.reset()
+            while (not done and n_steps<max_steps):
+                prev_envstate = envstate
+                action = self.predict(prev_envstate)
+
+                envstate, reward, done, info = self.env.step(action)
+
+                episode = [prev_envstate, action, reward, envstate, done]
+                self.remember(episode)
+
+                loss = self.learn() #fit model
+                if epoch >= n_epochs - 10:
+                    self.env.render()
+                n_steps += 1
+
+            dt = datetime.datetime.now() - self.start_time
+            t  = self.format_time(dt.total_seconds())
+            if epoch % self.show_every == 0:
+                results = f'Epoch: {epoch}/{n_epochs-1} | Loss: %.4f | Steps {n_steps} | Epsilon: %.3f | Time: {t}' % (loss, self.epsilon)
+                print(results)
+
+            #decay epsilon after each epoch
+            if self.action_policy == 'eg':
+                decay = self.explore_spec['EPSILON_DECAY']
+                self.epsilon = max(self.min_epsilon, decay*self.epsilon)
+    
+    # def temp_get_batch(self):
+    #     pr = cProfile.Profile()
+    #     pr.enable()
+    #     retval = self.get_batch()
+    #     pr.disable()
+    #     s = io.StringIO()
+    #     sortby = 'cumulative'
+    #     ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+    #     ps.print_stats(.05)
+    #     print(s.getvalue())
+    #     return retval
--- Deep Q Learning/Deep_Q_Demo.py
+++ Deep Q Learning/Deep_Q_Demo.py
@@ -1,25 +1,52 @@
 import gym
 from DQAgent import DQAgent
 
-env = gym.make('CartPole-v0')
+env = gym.make('MountainCar-v0')
 
-agent = DQAgent(env)
-agent.build_model(add_dropout=True)
+agent_opts = {
+                'BATCH_SIZE':             16,
+                'AGGREGATE_STATS_EVERY':   5, # not implemented
+                'SHOW_EVERY':              2,
+                'EPSILON_START':         .99,
+                'EPSILON_DECAY':         .99,
+                'DISCOUNT':              .90,
+                'MAX_STEPS':             200,
+                'MIN_EPSILON' :          .15,
+                'REPLAY_MEMORY_SIZE':    500,
+                'LEARNING_RATE':         0.05,
+                'ACTION_POLICY':         'softmax'
+            } 
+
+model_opts = {
+                'num_layers':      2,
+                'default_nodes':   20,
+                'dropout_rate':    0.5,
+                'add_dropout':     False,
+                'activation':      'linear',
+                'nodes_per_layer': [10, 10],
+            }
+
+agent = DQAgent(env, **agent_opts)
+agent.build_model(**model_opts)
+agent.train(n_epochs=800)
+agent.evaluate(5)
+env.close()
+print('Best Reward:', agent.best_reward)
 
 # envstate, action, reward, next_envstate, done
 
-for _ in range(5): #epoch
- obs = env.reset()
- done = False
- for _ in range(10): #steps
-   prev_state = obs
-   action = env.action_space.sample()
-   obs, reward, done, _ = env.step(action)
+# for _ in range(5): #epoch
+#  obs = env.reset()
+#  done = False
+#  for _ in range(10): #steps
+#    prev_state = obs
+#    action = env.action_space.sample()
+#    obs, reward, done, _ = env.step(action)
 
-   episode = [prev_state, action, reward, obs, done]
-   agent.remember(episode)
-   env.render()
+#    episode = [prev_state, action, reward, obs, done]
+#    agent.remember(episode)
+#    env.render()
 
-   if done:
-     print('Result:', reward)
-     break    
+#    if done:
+#      print('Result:', reward)
+#      break    
