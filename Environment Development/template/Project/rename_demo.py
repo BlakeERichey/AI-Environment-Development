7b86545a51f37bb571a09d92a169ee8566af3bac
Blake Richey <blake.e.richey@gmail.com>
2019-07-24 15:36:30 -0500

    Added demo for new environments to template

--- Environment Development/template/Project/rename_demo.py
+++ Environment Development/template/Project/rename_demo.py
@@ -3,5 +3,15 @@ import rename
 
 env = gym.make('rename-v0')
 
-action = env.action_space.sample()
-print(action)
+print('Observation Space:', env.observation_space)
+print('Available Actions:', env.action_space.n   )
+for epoch in range(1):
+  num_steps = 0
+  done      = False
+  envstate  = env.reset()
+  while not done and num_steps < 10: #perform action/step
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    env.render()
+
+env.close()
