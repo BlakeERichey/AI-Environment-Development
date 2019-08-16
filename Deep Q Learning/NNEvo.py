d5bfa77213661bfba7a5526710e341df490245ea
Blake Richey <blake.e.richey@gmail.com>
2019-08-16 17:14:17 -0500

    Added NNEvo. Incomplete, but file added

---
+++ Deep Q Learning/NNEvo.py
@@ -0,0 +1,21 @@
+import numpy as np
+class NNEvo:
+
+  def __init__(self, population, mxrt, crrt, layers, nodes_per_layer, generations, env)
+    pass
+
+  def create_nn(self, layers, nodes_per_layer):
+    input_layer_shape = self.env.observation_space #(9,1) [1,2,3,4,5,6,7,8,9]
+
+  def activation(self, val):
+    '''RELU'''
+    return max(0, val)
+
+
+create individual(NN)/population(multiple NNs)
+#mating pool/parent selection
+crossover/breed
+mutation
+forward propgation
+training
+activation
