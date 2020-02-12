783959a263a63d7764c1b6d172782d5a8f4c28b3
Blake <blake.e.richey@gmail.com>
2020-02-11 22:16:01 -0600

    Added actor critic and feed forward method.

--- Deep Q Learning/A3C/model.py
+++ Deep Q Learning/A3C/model.py
@@ -12,41 +12,84 @@ env = gym.make('Breakout-v0')
 print(env.unwrapped.get_action_meanings())
 
 class ActorCritic:
-  
+  '''
+    Create actor-critic models for A3C algorithm
+  '''
   def __init__(self,env, n_timesteps):
-    model = Sequential()
+    #Actor
+    actor = Sequential()
     #========== Convolutional Base ==============================
     #input layer
-    model.add(TimeDistributed( Conv2D(64, kernel_size=3, activation='relu'), \
+    actor.add(TimeDistributed( Conv2D(64, kernel_size=3, activation='relu'), \
       input_shape=(n_timesteps,) + env.observation_space.shape ))
 
-    model.add(TimeDistributed( Conv2D(32, kernel_size=3, activation='relu') ))
-    model.add(TimeDistributed(MaxPool2D(pool_size=(2,2))))
-    model.add(TimeDistributed( Conv2D(16, kernel_size=3, activation='relu') ))
-    model.add(TimeDistributed(MaxPool2D(pool_size=(2,2))))
-    model.add(TimeDistributed(Flatten()))
+    actor.add(TimeDistributed( Conv2D(32, kernel_size=3, activation='relu') ))
+    actor.add(TimeDistributed(MaxPool2D(pool_size=(2,2))))
+    actor.add(TimeDistributed( Conv2D(16, kernel_size=3, activation='relu') ))
+    actor.add(TimeDistributed(MaxPool2D(pool_size=(2,2))))
+    actor.add(TimeDistributed(Flatten()))
 
     #========== FCN ==============================
-    model.add(LSTM(32, return_sequences=True))
-    model.add(TimeDistributed(BatchNormalization()))
-    model.add(LSTM(16, return_sequences=True, dropout=.2))
-    model.add(LSTM(8, return_sequences=True))
-    model.add(TimeDistributed(Dense(env.action_space.n, activation='softmax')))
-    model.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['mse'])
+    actor.add(LSTM(32, return_sequences=True))
+    actor.add(TimeDistributed(BatchNormalization()))
+    actor.add(LSTM(16, return_sequences=True, dropout=.2))
+    actor.add(LSTM(8, return_sequences=True))
+    actor.add(TimeDistributed(Dense(env.action_space.n, activation='softmax')))
+    actor.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['mse'])
 
-    model.summary()
-    self.model = model
+    actor.summary()
+    self.actor = actor
+    
+    #Critic
+    critic = Sequential()
+    #========== Convolutional Base ==============================
+    #input layer
+    critic.add(TimeDistributed( Conv2D(64, kernel_size=3, activation='relu'), \
+      input_shape=(n_timesteps,) + env.observation_space.shape ))
+
+    critic.add(TimeDistributed( Conv2D(32, kernel_size=3, activation='relu') ))
+    critic.add(TimeDistributed(MaxPool2D(pool_size=(2,2))))
+    critic.add(TimeDistributed( Conv2D(16, kernel_size=3, activation='relu') ))
+    critic.add(TimeDistributed(MaxPool2D(pool_size=(2,2))))
+    critic.add(TimeDistributed(Flatten()))
+
+    #========== FCN ==============================
+    critic.add(LSTM(32, return_sequences=True))
+    critic.add(TimeDistributed(BatchNormalization()))
+    critic.add(LSTM(16, return_sequences=True, dropout=.2))
+    critic.add(TimeDistributed(Dense(1, activation='linear')))
+    critic.compile(loss="mse", optimizer=Adam(lr=0.001), metrics=['mse'])
+
+    critic.summary()
+    self.critic = critic
+
+  def feed_forward(self, ob):
+    '''
+      Used for easy computation of both actor and critics evalution of a state
+    '''
+    #actor determines action
+    qvals = self.actor.predict(np.expand_dims(ob, axis=0))
+    action = np.argmax(qvals[0][-1]) #action based on last time steps decision
+
+    #critic evaluates state value
+    state_value = self.critic.predict(np.expand_dims(ob, axis=0))[0][-1]
+
+    return action, state_value
+
 
 def run_game(steps=None, n_timesteps = 1):
+  '''
+    Runs games using a ActorCritic class obj as `agents` on a gym environment
+  '''
+
   ob = env.reset()
   ob = np.array([ob for _ in range(n_timesteps)])
   print('Reset.')
   done = False
   num_steps = 0
   while not done and (True,num_steps<steps)[steps is not None]:
-    qvals = model.predict(np.expand_dims(ob, axis=0))
-    action = np.argmax(qvals[0][-1]) #action based on last time steps decision
-    print(action)
+    action, state_value = agents.feed_forward(ob)
+    print(action, state_value)
     action = env.action_space.sample()
     envstate, reward, done, _ = env.step(action) 
 
@@ -57,8 +100,8 @@ def run_game(steps=None, n_timesteps = 1):
   env.close()
 
 
-num_steps = 200
-n_timesteps = 4
-model = ActorCritic(env, n_timesteps).model
+# num_steps = 200
+# n_timesteps = 4
+# agents = ActorCritic(env, n_timesteps) 
 
-run_game(num_steps, n_timesteps)
+# run_game(num_steps, n_timesteps)
